d9cea674606ca75d4f428ee9ec664475efe108d1
Kate deBethune <kdebethune@gmail.com>
2015-09-02 10:13:24 -0400

    cleaned up fb.js a tad

--- js/fb.js
+++ js/fb.js
@@ -1,20 +1,31 @@
+/*******************************/
+/* 								
+/* CSCI-E32 September 2, 2015
+/* Assignment 0
+/* fizzbuzz / fork / pull
+/* 
+/* Kate de Bethune
+/* 
+/***************************/
+
 'use strict';
 
 function fizzBuzz(x) {  
-	 var lim = x,   y = 1;   
+	var lim = x, y = 1;   
 	for (; y <= lim; y++) {    
-		 if (y % 3 == 0 && y % 5 == 0) {      
-			 console.log('FizzBuzz');     
+		if (y % 3 == 0 && y % 5 == 0) {      
+			console.log('FizzBuzz');     
 		}      
 		else if (y % 3 == 0) {       
 			console.log('Fizz');     
-		}      else if (y % 5 == 0) {       
+		}      
+		else if (y % 5 == 0) {       
 			console.log('Buzz');     
 		}     
-		 else {      
+		else {      
 		 	console.log(y);     
 		}   
 	} 
 } 
 
-fizzBuzz(50); 
+fizzBuzz(120); 
